a5f33700ac20f726b3d28bf362354438430e61e7
emanuelfeld <emanuelfeld@gmail.com>
2015-07-16 14:18:54 -0400

    Style markers red circles

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
-  var tiles = L.tileLayer('http://a.tiles.mapbox.com/v3/tmcw.map-l1m85h7s/{z}/{x}/{y}.png', {
+var tiles = L.tileLayer('http://a.tiles.mapbox.com/v3/tmcw.map-l1m85h7s/{z}/{x}/{y}.png', {
     maxZoom: 18
-  });
+});
 
 function popUp(feature, layer) {
   var key, val;
@@ -17,10 +17,22 @@ function popUp(feature, layer) {
   layer.bindPopup(content.join("<br />"));
 }
 
-  var willardLayer = new L.GeoJSON.AJAX("map_willard.geojson", {onEachFeature:popUp});
+function markerStyle(feature, latlng) {
+                return new L.CircleMarker(latlng, {
+                    radius: 7,
+                    fillColor: "#e11111",
+                    color: "#000",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.6
+                });
+}
 
+var willardLayer = new L.GeoJSON.AJAX("map_willard.geojson", {pointToLayer: markerStyle, onEachFeature:popUp});
+var curtisLayer = new L.GeoJSON.AJAX("map_curtis.geojson", {pointToLayer: markerStyle, onEachFeature:popUp});
 var map = L.map('map').setView([38.8993488, -77.0145665], 12);
 
-  tiles.addTo(map);
-  willardLayer.addTo(map);
+tiles.addTo(map);
+willardLayer.addTo(map);
+curtisLayer.addTo(map);
 
